93f268d14f220c88787842cb5a7ea795f0de0d0f
maxshashoua@gmail.com <maxshashoua@gmail.com>
2019-12-03 10:13:59 -0500

    Updates

---
+++ precache-manifest.780b5f4409bd202b6db5ebf600ab9c96.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219034bacd235fbc8a79885dbfe67e69",
+    "url": "/moic/index.html"
+  },
+  {
+    "revision": "fc4552541052c19e30ad",
+    "url": "/moic/static/css/main.375813e3.chunk.css"
+  },
+  {
+    "revision": "09358a564ced1c1090e5",
+    "url": "/moic/static/js/2.5dc4bed9.chunk.js"
+  },
+  {
+    "revision": "fc4552541052c19e30ad",
+    "url": "/moic/static/js/main.4b913862.chunk.js"
+  },
+  {
+    "revision": "10d521ae37adac5bf991",
+    "url": "/moic/static/js/runtime-main.9ba1b73b.js"
+  },
+  {
+    "revision": "c4e446ded7beaa99be366f1f541840f9",
+    "url": "/moic/static/media/About.c4e446de.png"
+  },
+  {
+    "revision": "38c572e5bffd5704f8729dc69b78e0e6",
+    "url": "/moic/static/media/Museum-of-ice-cream_ice-cream_churro-churro.38c572e5.png"
+  },
+  {
+    "revision": "b0445125f1164f87c1bd0a4400999ee4",
+    "url": "/moic/static/media/Museum-of-ice-cream_ice-cream_malt-shake.b0445125.png"
+  },
+  {
+    "revision": "cd7214e81dcd249d36a1d98994c8d739",
+    "url": "/moic/static/media/Museum-of-ice-cream_ice-cream_nana-banana.cd7214e8.png"
+  },
+  {
+    "revision": "c636515ddfe8b0cac9766158718d9e96",
+    "url": "/moic/static/media/Museum-of-ice-cream_ice-cream_pb-&-p.c636515d.png"
+  },
+  {
+    "revision": "870720ad060eb33ed836db4f99c81c0d",
+    "url": "/moic/static/media/Museum-of-ice-cream_ice-cream_pinata-pinata.870720ad.png"
+  },
+  {
+    "revision": "c9f309b3d47969ecac64a77a6c672594",
+    "url": "/moic/static/media/NexaBold.c9f309b3.otf"
+  },
+  {
+    "revision": "bf6f9e7edbf0ada23f9e27d69b985e6f",
+    "url": "/moic/static/media/PINTS-02.bf6f9e7e.png"
+  },
+  {
+    "revision": "e6f10070c2b92cdccaba2ee44890e476",
+    "url": "/moic/static/media/Pint+Stacks_Updated.e6f10070.png"
+  },
+  {
+    "revision": "b42ab2fb312555fe52dc088d5e87f657",
+    "url": "/moic/static/media/SF-08-min.b42ab2fb.png"
+  },
+  {
+    "revision": "ccb66e786cb4c51be64b630aefdacfd9",
+    "url": "/moic/static/media/SF-10-min.ccb66e78.png"
+  },
+  {
+    "revision": "9b063b4f051df5722c9c59627b834890",
+    "url": "/moic/static/media/SF-11-min.9b063b4f.png"
+  },
+  {
+    "revision": "26dcf0ea5f75dc29f1844a475664344c",
+    "url": "/moic/static/media/Visit+NYC-05.26dcf0ea.png"
+  },
+  {
+    "revision": "2d4eed4ec72385e1366d8ba1baf2bd3d",
+    "url": "/moic/static/media/Visit+NYC-06.2d4eed4e.png"
+  },
+  {
+    "revision": "85fbf0af48faaaa37dab6517cd093ee6",
+    "url": "/moic/static/media/icecreamcursor.85fbf0af.png"
+  },
+  {
+    "revision": "5f20ee708954eed53453f8a51ab219a5",
+    "url": "/moic/static/media/ig_logo_pink.5f20ee70.png"
+  },
+  {
+    "revision": "f747e749b1fcc5b0814c281fce2fff40",
+    "url": "/moic/static/media/ig_logo_red.f747e749.png"
+  },
+  {
+    "revision": "a8d5dc2a6599dc702a45eb37e462d6be",
+    "url": "/moic/static/media/moic-logo-white-one-line.a8d5dc2a.png"
+  },
+  {
+    "revision": "6d9dd672727a6c8c7a0785f776a9b67d",
+    "url": "/moic/static/media/museum-of-ice-cream_ice-cream_queen-bee.6d9dd672.png"
+  },
+  {
+    "revision": "760a1f0451706e5722f618814a9e107b",
+    "url": "/moic/static/media/nyc.760a1f04.png"
+  },
+  {
+    "revision": "dc4954a316dad86646755702b59dcfb5",
+    "url": "/moic/static/media/sf-13-min.dc4954a3.png"
+  }
+]);
